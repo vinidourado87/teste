18a71e1191de2b31a22c88798fa8a9a6d2192999
Weliff Lima <weliff.14@gmail.com>
2017-03-24 20:53:43 -0300

    inicial

---
+++ src/main/java/com/bmape/BmapeCoreServicesApplication.java
@@ -0,0 +1,12 @@
+package com.bmape;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmapeCoreServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BmapeCoreServicesApplication.class, args);
+	}
+}
